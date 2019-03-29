a83d99acd7c41f185147fb4dbb05ccae2813de9b
Michael Cooley <9083275+MidoriKami@users.noreply.github.com>
2019-03-29 12:16:32 -0700

    Added Comments for Usability

--- DuplicantPicker.py
+++ DuplicantPicker.py
@@ -20,6 +20,16 @@ def ProcessDuplicant(x1, y1, x2, y2, c1, c2):
         gastrophobia = imagesearcharea("Gastrophobia.png", x1, y1, x2, y2)
         pacifist = imagesearcharea("Pacifist.png", x1, y1, x2, y2)
 
+#   All values are screen coordinates
+#   x1, y1, x2, y2, c1, c2
+#
+#   x1 -    Top Left X Coordinate for the "Traits Section" of the duplicant
+#   y1 -    Top Left Y Coordinate for the "Traits Section" of the duplicant
+#   x2 -    Bottom Right X Coordinate for the "Traits Section" of the duplicant
+#   y2 -    Bottom Right Y Coordinate for the "Traits Section" of the duplicant
+#   c1 -    X Coordinate for "Suffle Button"
+#   c2 -    Y Coordinate for "Shuffle Button"
+        
 ProcessDuplicant(870, 510, 1040, 560, 1000, 425)
 ProcessDuplicant(1270, 510, 1470, 560, 1400, 425)
 ProcessDuplicant(1670, 510, 1870, 560, 1800, 425)
