c2b4e9f9237d3f08f2f81452dd422279481b01b6
Michael Cooley <9083275+MidoriKami@users.noreply.github.com>
2019-03-31 15:17:47 -0700

    Various Improvements
    Added Oxygen Not Included process execution check.
    Checks to make sure ONI is running before attempting anything.
    
    Threaded the Duplicant Picker, It will now reshuffle all duplicants at the same time.

--- DuplicantPicker.py
+++ DuplicantPicker.py
@@ -1,4 +1,15 @@
+try:
+    from PIL import Image
+except ImportError:
+    import Image
+import pytesseract
+from multiprocessing.dummy import Pool as ThreadPool
+import numpy as np
 from imagesearch import *
+import sys
+import psutil
+import win32gui
+import win32con
 
 def ImageFound(image):
     if image[0] != -1 :
@@ -6,8 +17,9 @@ def ImageFound(image):
     else:
         return False
 
+
+
 def ProcessDuplicant(x1, y1, x2, y2, c1, c2):
-    # Find Divers Lungs
     counter = 0
     divers = imagesearcharea("DiversLungs.png", x1, y1, x2, y2)
     gastrophobia = imagesearcharea("Gastrophobia.png", x1, y1, x2, y2)
@@ -20,16 +32,27 @@ def ProcessDuplicant(x1, y1, x2, y2, c1, c2):
         gastrophobia = imagesearcharea("Gastrophobia.png", x1, y1, x2, y2)
         pacifist = imagesearcharea("Pacifist.png", x1, y1, x2, y2)
 
-#   All values are screen coordinates
-#   x1, y1, x2, y2, c1, c2
-#
-#   x1 -    Top Left X Coordinate for the "Traits Section" of the duplicant
-#   y1 -    Top Left Y Coordinate for the "Traits Section" of the duplicant
-#   x2 -    Bottom Right X Coordinate for the "Traits Section" of the duplicant
-#   y2 -    Bottom Right Y Coordinate for the "Traits Section" of the duplicant
-#   c1 -    X Coordinate for "Suffle Button"
-#   c2 -    Y Coordinate for "Shuffle Button"
-        
-ProcessDuplicant(870, 510, 1040, 560, 1000, 425)
-ProcessDuplicant(1270, 510, 1470, 560, 1400, 425)
-ProcessDuplicant(1670, 510, 1870, 560, 1800, 425)
+if "OxygenNotIncluded.exe" in (p.name() for p in psutil.process_iter()) :
+    pool = ThreadPool(13)
+
+    whnd = win32gui.FindWindow(None, "Oxygen Not Included")
+
+    win32gui.ShowWindow(whnd, win32con.SW_MAXIMIZE)
+    win32gui.SetForegroundWindow(whnd)
+
+
+    #   All values are screen coordinates
+    #   x1, y1, x2, y2, c1, c2
+    #
+    #   x1 -    Top Left X Coordinate for the "Traits Section" of the duplicant
+    #   y1 -    Top Left Y Coordinate for the "Traits Section" of the duplicant
+    #   x2 -    Bottom Right X Coordinate for the "Traits Section" of the duplicant
+    #   y2 -    Bottom Right Y Coordinate for the "Traits Section" of the duplicant
+    #   c1 -    X Coordinate for "Suffle Button"
+    #   c2 -    Y Coordinate for "Shuffle Button"
+    pool.starmap(ProcessDuplicant, [ 
+        [870, 510, 1040, 560, 1000, 425], 
+        [1270, 510, 1470, 560, 1400, 425],
+        [1670, 510, 1870, 560, 1800, 425]
+    ])
+
